aceecd3c568db836c4892fc095f8d7cc58dc87ae
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2021-04-08 15:09:32 -0400

    Configure additional error pages for hackathon site.

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 import { CfnDistribution } from '@aws-cdk/aws-cloudfront'
 import { PolicyStatement } from '@aws-cdk/aws-iam'
-import { BuildEnvironmentVariable, BuildEnvironmentVariableType } from '@aws-cdk/aws-codebuild'
+import { BuildEnvironmentVariable } from '@aws-cdk/aws-codebuild'
 
 export interface IProjectDefaults {
   readonly stackNamePrefix: string
@@ -45,6 +45,12 @@ export const Config: IConfig = {
     indexFilename: 'index.shtml',
     errorConfig: [
       {
+        errorCode: 400,
+        responseCode: 400,
+        responsePagePath: '/error/400.shtml',
+        errorCachingMinTtl: 300,
+      },
+      {
         errorCode: 403,
         responseCode: 403,
         responsePagePath: '/error/403.shtml',
@@ -54,6 +60,36 @@ export const Config: IConfig = {
         errorCode: 404,
         responseCode: 404,
         responsePagePath: '/error/404.shtml',
+        errorCachingMinTtl: 300,
+      },
+      {
+        errorCode: 405,
+        responseCode: 405,
+        responsePagePath: '/error/405.shtml',
+        errorCachingMinTtl: 300,
+      },
+      {
+        errorCode: 500,
+        responseCode: 500,
+        responsePagePath: '/error/500.shtml',
+        errorCachingMinTtl: 300,
+      },
+      {
+        errorCode: 501,
+        responseCode: 501,
+        responsePagePath: '/error/501.shtml',
+        errorCachingMinTtl: 300,
+      },
+      {
+        errorCode: 503,
+        responseCode: 503,
+        responsePagePath: '/error/503.shtml',
+        errorCachingMinTtl: 300,
+      },
+      {
+        errorCode: 504,
+        responseCode: 504,
+        responsePagePath: '/error/504.shtml',
         errorCachingMinTtl: 300,
       },
     ],
